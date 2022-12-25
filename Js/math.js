2daeae4896bb04aea2d6391683e4fc1038396d48
Wild-bit <1397119446@qq.com>
2022-12-25 22:58:46 +0800

    feat: 力扣303 区域和检索 - 数组不可变 技巧前缀和

--- Js/math.js
+++ Js/math.js
@@ -38,3 +38,43 @@ const search = function (nums, target) {
 // 目标值则在mid的右边，则搜索区间需要变为[mid + 1,right],否则变为[left, mid-1]
 
 // 参考链接：https://leetcode.cn/problems/binary-search/solution/er-fen-cha-zhao-by-leetcode-solution-f0xw/
+
+/**
+ * 力扣303. 区域和检索 - 数组不可变
+ * 关键字：前缀和技巧
+ * 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+ *
+ * 输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ */
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  const len = nums.length
+  const prenums = new Array(len + 1).fill(0)
+  for (let i = 1; i < prenums.length; i++) {
+    prenums[i] = prenums[i - 1] + nums[i - 1]
+  }
+  this.prenums = prenums
+}
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  return this.prenums[right + 1] - this.prenums[left]
+}
+// 题解：https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247494095&idx=2&sn=19a2609f33eadbbda1f6b75e2298d931&scene=21#wechat_redirect
